5910ab61ebb31b62e8406b7bee19d12a9b2d7fde
abit salihu <abiti.salihu@gmail.com>
2023-01-01 15:36:16 +0100

    make the coppy button functional to copy the quote to the clipboard

--- script.js
+++ script.js
@@ -4,42 +4,44 @@ const quoteAuthor = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 
+console.log(twitterBtn, newQuoteBtn);
 
 let apiQuotes = [];
 // Show New Quote
-function newQuote(){
-    //  Pick  a random quote from the api 
-    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    if(!quote.author){
-        quoteAuthor.textContent='Unkown';
-    }else{
-        quoteAuthor.textContent=quote.author;
-    }
-  
-    if(quote.text.length>50){
-        quoteText.classList.add('long-quote');
-    }else{
-        quoteText.classList.remove('long-quote');
-    }
-    quoteText.textContent=quote.text;
+function newQuote() {
+  //  Pick  a random quote from the api
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  if (!quote.author) {
+    quoteAuthor.textContent = "Unkown";
+  } else {
+    quoteAuthor.textContent = quote.author;
+  }
+
+  if (quote.text.length > 50) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 }
 
-// Fetching the API 
-async function getQuotes(){
-    const apiUrl = 'https://type.fit/api/quotes';
-    try {
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        newQuote();
-    } catch (error) {
-        // Catch Error Here
-
-    }
-
+// Fetching the API
+async function getQuotes() {
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
 }
 
 // Event Listener
-newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(quoteText.textContent);
+});
+newQuoteBtn.addEventListener("click", newQuote);
 
 // On Load
 getQuotes();
