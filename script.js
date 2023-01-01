8afb7f9975632a1185d6249e1670a9ab45132250
Dheeraj Yadav <dheeraj4567yadav@gmail.com>
2023-01-02 00:27:19 +0530

    changes in script file

--- script.js
+++ script.js
@@ -4,28 +4,26 @@ const quoteAuthor = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 
-console.log(twitterBtn, newQuoteBtn);
 
 let apiQuotes = [];
 // Show New Quote
-function newQuote() {
-  //  Pick  a random quote from the api
-  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  if (!quote.author) {
-    quoteAuthor.textContent = "Unkown";
-  } else {
-    quoteAuthor.textContent = quote.author;
-  }
-
-  if (quote.text.length > 50) {
-    quoteText.classList.add("long-quote");
-  } else {
-    quoteText.classList.remove("long-quote");
-  }
-  quoteText.textContent = quote.text;
+function newQuote(){
+    //  Pick  a random quote from the api 
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    if(!quote.author){
+        quoteAuthor.textContent='Unkown';
+    }else{
+        quoteAuthor.textContent=quote.author;
+    }
+  
+    if(quote.text.length>50){
+        quoteText.classList.add('long-quote');
+    }else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent=quote.text;
 }
 
-<<<<<<< HEAD
 // Fetching the API 
 async function getQuotes(){
     const apiUrl = 'https://type.fit/api/quotes';
@@ -38,18 +36,6 @@ async function getQuotes(){
 
     }
 
-=======
-// Fetching the API
-async function getQuotes() {
-  const apiUrl = "https://type.fit/api/quotes";
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch (error) {
-    // Catch Error Here
-  }
->>>>>>> 041905cf3aaab41792a83a2e4a2dcf18d0b1b35d
 }
 
 // Event Listener
